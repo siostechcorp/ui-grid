a8b33c84c3ef972b3898c94d5fde7b0b20d02052
siosTomBeckenhauer <tom.beckenhauer@us.sios.com>
2016-01-12 13:57:10 -0500

    Action pane button for Vm Grid Work and Grid Row Height 40px work  https://github.com/siostechcorp/cloud-orchestrator-ui/commit/6a7065b12191255cabd2d75880d0bd267b24d8f1
     Grid Row Header Height 40px work
     https://github.com/siostechcorp/cloud-orchestrator-ui/commit/c2c41da9e25af455f3d77d9d19d9eba70f6354f3
    
    not sure how  this didn't get in there

--- build/ng-grid.debug.js
+++ build/ng-grid.debug.js
@@ -1309,7 +1309,7 @@ var ngGrid = function ($scope, options, sortService, domUtilityService, $filter,
         groupsCollapsedByDefault: true,
         
         //The height of the header row in pixels.
-        headerRowHeight: 30,
+        headerRowHeight: 40,
 
         //Define a header row template for further customization. See github wiki for more details.
         headerRowTemplate: undefined,
