db8df375aa202ca98b43010896df4d19501c8c27
siosTomBeckenhauer <tom.beckenhauer@us.sios.com>
2016-01-12 14:03:25 -0500

    ng grid issue resolved  https://github.com/siostechcorp/cloud-orchestrator-ui/commit/d07d9e99099d22b25dc76ff18559c1865ef61179

--- build/anchor-last-column.js
+++ build/anchor-last-column.js
@@ -1,35 +1,106 @@
-function anchorLastColumn () {
-    var self = this;
-    self.grid = null;
-    self.scope = null;
-    self.services = null;
-    self.init = function (scope, grid, services) {
-      self.grid = grid;
-      self.scope = scope;
-      self.services = services;
+function anchorLastColumn() {
+	var self = this;
+	self.grid = null;
+	self.scope = null;
+	self.services = null;
+	self.init = function(scope, grid, services) {
+		self.grid = grid;
+		self.scope = scope;
+		self.services = services;
+		var lastSum = 0;
+		var startSum = 0;
+		var lastCol;
+		var gridWidth;
 
-      self.scope.$watch('isColumnResizing', function (newValue, oldValue) {
-        if (newValue === false && oldValue === true) { //on stop resizing
-          var gridWidth = self.grid.rootDim.outerWidth;
-      	  var viewportH = self.scope.viewportDimHeight();
-      	  var maxHeight = self.grid.maxCanvasHt;
-      	  if(maxHeight > viewportH) { // remove vertical scrollbar width
-      		  gridWidth -= self.services.DomUtilityService.ScrollW;
-      	  }
-  
-          var cols = self.scope.columns;
-          var col = cols[cols.length - 1]; // last column
-          var sum = 0;
-          for(var i = 0; i < cols.length - 1; i++) {
-        		if(cols[i].visible) {
-        			sum += cols[i].width;
-        		}
-          }
-  
-          if(sum + col.minWidth <= gridWidth) {
-            col.width = gridWidth - sum; // the last gets the remaining
-          }
-        }
-      });
-    }
+		self.scope.$watch('isColumnResizing', function(newValue, oldValue) {
+
+			if (newValue === false && oldValue === true) {
+
+				lastSum = 0;
+				var cols = self.scope.columns;
+
+				var col = cols[cols.length - 1]; // last column
+				gridWidth = self.grid.rootDim.outerWidth;
+
+				for ( var i = 0; i < cols.length - 1; i++) {
+					if (cols[i].visible) {
+						lastSum += cols[i].width;
+					}
+				}
+
+				lastWidthAtEnd = col.width;
+				console.log('Resizing stop------ ', lastSum);
+
+				if (lastCol != undefined) {
+					var diff = gridWidth - lastSum;
+					if (lastSum < startSum)
+						lastCol.width = diff;
+
+				}
+
+			}
+
+			if (newValue === true && oldValue === false) { // on stop resizing
+
+				gridWidth = self.grid.rootDim.outerWidth;
+				var viewportH = self.scope.viewportDimHeight();
+				var maxHeight = self.grid.maxCanvasHt;
+				console.log("Test scroll width ------------ ", self.services);
+
+				if (maxHeight > viewportH) { // remove vertical scrollbar
+					// width
+					console.log("Test scroll width ------------ ",
+							self.services);
+					gridWidth -= self.services.DomUtilityService.ScrollW
+				}
+				startSum = 0;
+				var cols = self.scope.columns;
+
+				var col = cols[cols.length - 1]; // last column
+
+				for ( var i = 0; i < cols.length - 1; i++) {
+					if (cols[i].visible) {
+						startSum += cols[i].width;
+					}
+					if (i === cols.length - 2)
+						lastWidthAtStart = cols[i + 1].width;
+				}
+
+				console.log('Resizing start------ ', startSum);
+
+				gridWidth = self.grid.rootDim.outerWidth;
+				if (startSum + col.minWidth <= gridWidth) {
+					col.width = gridWidth - startSum; // the last gets the
+					lastCol = col;
+					// remaining
+				}
+			} else if (newValue === true) {
+				
+				gridWidth = self.grid.rootDim.outerWidth;
+
+				startSum = 0;
+				var cols = self.scope.columns;
+
+				var col = cols[cols.length - 1]; // last column
+
+				for ( var i = 0; i < cols.length - 1; i++) {
+					if (cols[i].visible) {
+						startSum += cols[i].width;
+					}
+
+				}
+
+				console.log('Resizing start------ ', startSum);
+
+				gridWidth = self.grid.rootDim.outerWidth;
+				if (startSum + col.minWidth <= gridWidth) {
+					col.width = gridWidth - startSum; // the last gets the
+					lastCol = col;
+					// remaining
+				}
+			}
+
+		});
+
+	}
 }
